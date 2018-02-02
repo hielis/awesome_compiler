3d34876ee3ddba650f7144ea326b8dab38e8d90f
john <hielis@localhost.localdomain>
2018-02-02 08:26:23 +0100

    expression typing is done

--- typing.ml
+++ typing.ml
@@ -1,14 +1,14 @@
-
 open Ttree
-open ptree.Ptree
+open Ptree
 (* utiliser cette exception pour signaler une erreur de typage *)
 exception Error of string
 
 let to_string (a : loc) = failwith "Implement me";;
-let error_message a l = a ^ (to_string l);;
+let error_message a l = (a ^ (to_string l));;
 
-let eq_type tref = function |Ttypenull -> not (tref = Tvoid)
-                            |Tvoid -> not (tref = Tint || tref = Ttypenull)
+
+let eq_type tref = function |Ttypenull -> not (tref = Tvoidstar)
+                            |Tvoidstar -> not (tref = Tint || tref = Ttypenull)
                             |Tint -> (tref = Ttypenull) || (tref = Tint)
                             |Tstructp(_) as a-> (a = tref) || (tref = Ttypenull);;
 (*checking structure type is not safe yet*)
@@ -20,15 +20,16 @@ let string_of_type = function
   | Tint       -> "int"
   | Tstructp x -> "struct " ^ x.str_name ^ " *"
   | Tvoidstar  -> "void*"
-  | Ttypenull  -> "typenull"
+  | Ttypenull  -> "typenull";;
 
 let program p =
-  let function_table = Hashtbl.create 64 in
+  let function_table = Hashtbl.create 64 in (* An hashtable with all the functions ids as keys binded to (typ list * typ) à savoir les types des arguments et le type de retour *)
+
+  let struct_table = Hashtbl.create 64 in (*An hashtable matching the structs id with their structures *)
 
 
   let rec type_expr exp = match exp.expr_node with
-    |Econst(0) -> {expr_node : Econst(0);
-                              expr_typ : Ttypenull}
+    |Econst(a) when (Int32.to_int a = 0)-> {expr_node : Econst(a); expr_typ : Ttypenull}
     |Econst(a)-> {expr_node : Econst(a);
                   expr_typ : Tint}
     |Eunop(b, e) ->
@@ -57,7 +58,6 @@ let program p =
                                      expr_typ : Tint}
       |_, _, _ -> raise Error(error_message "This expression is non-sensical, go back to primary school, nerd" exp.expr_loc)
       )
-    (*Reste Ecall, Esizeof, Eright, Eassign *)
        |Eright(lv) ->
          (match lv with
           |Lident(i) -> {expr_node : Eaccess_local(i.id);
@@ -67,7 +67,7 @@ let program p =
                                             field_typ : ep.typ});
              expr_typ : ep.typ}
          )
-       |Eassign(v, e1) -> 
+       |Eassign(v, e1) ->
          let e1p = type_expr e1 in
          (match v with
           |Lident(i) -> {expr_node : Eassign_local(i.id, e1p);
@@ -75,7 +75,7 @@ let program p =
           |Larrow(e2, i) -> failwith "I do not understand"
          )
 
-       |Ecall(i, l) -> 
+       |Ecall(i, l) ->
          let (args, ret_typ) = (try (Hashtbl.find function_table i.id)
                                 with Not_found -> raise Error(error_message "Function unknown" exp.expr_loc)) in
          let compare b ep dv = match b, ep.expr_typ, dv with
@@ -88,6 +88,11 @@ let program p =
          if(test) then {expr_node : Ecall(i.id, lp);
                         expr_typ : ret_typ}
          else raise Error(error_message "Function was used with the wrong types" exp.expr_loc)
+
+       |Esizeof(i) ->
+         let s = (try Hashtbl.find struct_table (i.id) with Not_found -> raise Error(error_message "The struct used is unbound" exp.expr_loc)) in
+         {expr_node : Esizeof(s);
+         expr_typ : Tint}
        |_ -> failwith  "Not finished"
-       in
+       in       
        ;;
